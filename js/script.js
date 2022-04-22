a31ee789d575d7d498d83435c6b8b1242e4166ee
ArtemAndZakhar <69362178+ArtemAndZakhar@users.noreply.github.com>
2022-04-22 12:03:03 +0300

    1 lesson_1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const contents = document.querySelectorAll('.program-line__content');
-contents.forEach((elem)=> {
+contents.forEach((elem, index)=> {
     const title = elem.querySelector('.program-line__title');
     const descr = elem.querySelector('.program-line__descr');
     title.addEventListener('click', () =>{
-        delActive();
-        descr.classList.add('active');
+        delActive(index);
+        descr.classList.toggle('active');
     })
 })
-const delActive = ()=> {
-    contents.forEach((elem)=> {
-        elem.querySelector('.program-line__descr').classList.remove('active');
+const delActive = (indexActive)=> {
+    contents.forEach((elem, index)=> {
+        const descr = elem.querySelector('.program-line__descr');
+        if (indexActive !== index) descr.classList.remove('active');
     })
 }
 
